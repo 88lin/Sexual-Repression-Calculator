852f6e151b67302598d68adf898da2769ef71bfe
你好呀 <431761794@qq.com>
2025-10-10 20:14:45 +0800

    解决与cf部署不兼容问题

--- api/index.ts
+++
@@ -1,3 +0,0 @@
-import app from '../src/server/app.vercel.ts';
-
-export default app.fetch;
--- src/server/app.vercel.ts
+++
@@ -1,8 +0,0 @@
-import { Hono } from "hono";
-import { setupRoutes } from "./routes";
-
-const app = new Hono();
-
-setupRoutes(app);
-
-export default app;
